7a8b5b1075990a3fb0d570afc83ef748ca4ef14b
Craig Dennis <craigsdennis@gmail.com>
2022-08-20 22:44:43 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Snappy
+
+Simple little helper to access the HTML of a page after any dynamic JavaScript rendering has happened. It does this in a dorky way of waiting for requests to finish (therefore if no web requests are made this won't work. Sorry, but you should just right click and View Source at that point.)
+
+This uses [Playwright](https://playwright.dev/)
+
+## Usage
+
+```bash
+npm install
+node snappy.js <URL>
+```
+
+This will output the HTML to stdout to pipe to other applications or files
+
+```bash
+node snappy.js spacejam > static.html
+```
---
+++ snappy.js
@@ -0,0 +1,33 @@
+const { chromium } = require("playwright");
+
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+async function snappy(url) {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto(url);
+  function printHtmlAndExit() {
+    page.content().then((html) => {
+      console.log(html);
+      browser.close();
+    });
+  }
+  // When all requests have finished finishing. This is dorky.
+  page.on("requestfinished", debounce(printHtmlAndExit));
+}
+
+const url = process.argv[2];
+if (url === undefined) {
+  console.error(`Usage: node snappy.js <URL>`);
+  process.exit(1);
+}
+snappy(url);
